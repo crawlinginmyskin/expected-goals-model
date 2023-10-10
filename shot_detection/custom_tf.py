49494c16011a650f1037ab40b05b91d37097fba5
crawlinginmyskin <fzietara2137@gmail.com>
2023-10-10 20:39:24 +0200

    fix pathow + nowa architektura modelu

--- shot_detection/custom_tf.py
+++ shot_detection/custom_tf.py
@@ -7,7 +7,6 @@ from random import sample
 from tqdm import tqdm
 import logging
 from PIL import Image
-from time import sleep
 
 tf.get_logger().setLevel(logging.ERROR)
 
@@ -24,8 +23,7 @@ SHOTS_PATH = r'C:\Users\fziet\OneDrive\Pulpit\szkola\inzynierka\shot_detection\d
 def load_and_preprocess_image(image_path):
     image = Image.open(image_path)
     image.load()
-    image = image.resize((width, height))
-    return np.asarray(image, dtype="uint8")
+    return np.asarray(image.resize([width, height]), dtype="uint8")
 
 
 df_labels = pd.read_csv('labels.csv', index_col=None)
@@ -41,18 +39,21 @@ sciezki = df_labels['path'].tolist()
 ile = len(sciezki)
 for i, j in tqdm(enumerate(sciezki), total=ile):
     if i >= ile - 2:
-        break
-    picture_path = f"{DATASET_PATH}\\{j.replace('.txt', '.jpg')}"
-    picture_path1 = f"{DATASET_PATH}\\{sciezki[i+1].replace('.txt', '.jpg')}"
-    picture_path2 = f"{DATASET_PATH}\\{sciezki[i+2].replace('.txt', '.jpg')}"
-
-    sequence = np.array([load_and_preprocess_image(picture_path),
-                         load_and_preprocess_image(picture_path1),
-                         load_and_preprocess_image(picture_path2)])
-    if j in shots_files or sciezki[i+1] in shots_files or sciezki[i+2] in shots_files:
-        sequences_true.append(sequence)
-    else:
-        sequences_false.append(sequence)
+        continue
+    numer1 = j.split('_')[1]
+    numer2 = sciezki[i + 1].split('_')[1]
+    numer3 = sciezki[i + 2].split('_')[1]
+    if numer1 == numer2 == numer3:
+        picture_path = f"{DATASET_PATH}\\{j.replace('.txt', '.jpg')}"
+        picture_path1 = f"{DATASET_PATH}\\{sciezki[i + 1].replace('.txt', '.jpg')}"
+        picture_path2 = f"{DATASET_PATH}\\{sciezki[i + 2].replace('.txt', '.jpg')}"
+        sequence = np.array([load_and_preprocess_image(picture_path),
+                             load_and_preprocess_image(picture_path1),
+                             load_and_preprocess_image(picture_path2)])
+        if j in shots_files or sciezki[i+1] in shots_files or sciezki[i+2] in shots_files:
+            sequences_true.append(sequence)
+        else:
+            sequences_false.append(sequence)
 
 
 sequences = np.array(sequences_true + sample(sequences_false, len(sequences_true)))
@@ -69,41 +70,35 @@ train_dataset = sequences_dataset.take(train_size)
 val_dataset = sequences_dataset.skip(train_size)
 '''
 model = models.Sequential([
-    layers.Conv3D(64, (3, 3, 3), strides=(3, 3, 1), activation='relu', padding='same',
+    layers.Conv3D(32, (3, 3, 3), strides=(3, 3, 1), activation='relu', padding='same',
                   input_shape=(frames, height, width, channels)),
-    layers.MaxPool3D((2, 2, 1), strides=(2, 2, 1), padding='same',),
-    layers.BatchNormalization(),
+    layers.Conv3D(32, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
+    layers.MaxPool3D((2, 2, 2), strides=(2, 2, 2), padding='same'),
 
-    layers.Conv3D(128, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
-    layers.MaxPool3D((3, 3, 3), strides=(2, 2, 2), padding='same'),
+    layers.Conv3D(64, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
+    layers.Conv3D(64, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
+    layers.MaxPool3D((2, 2, 2), strides=(2, 2, 2), padding='same'),
 
-    layers.Conv3D(192, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
-    layers.Conv3D(192, (3, 3, 3),  strides=(2, 2, 3), padding='same', activation='relu'),
-    layers.MaxPool3D((3, 3, 3), strides=(2, 2, 2), padding='same'),
-
-    layers.Conv3D(192, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
-    layers.Conv3D(192, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
-    layers.MaxPool3D((2, 2, 1), strides=(2, 2, 1), padding='same'),
-
-    layers.Conv3D(192, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
-    layers.Conv3D(192, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
-    layers.MaxPool3D((2, 2, 1), strides=(2, 2, 1), padding='same'),
+    layers.Conv3D(64, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
+    layers.Conv3D(64, (3, 3, 3),  strides=(3, 3, 1), padding='same', activation='relu'),
+    layers.MaxPool3D((2, 2, 2), strides=(2, 2, 2), padding='same'),
 
     layers.Flatten(),
-
-    layers.Dense(128, activation='relu'),
     layers.Dense(128, activation='relu'),
     layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
     layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer='SGD',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 print('zkompilowano model')
-model.fit(x=sequences, y=labels_reshaped,
-          epochs=10,
-          validation_split=0.2)
+
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, start_from_epoch=100)
+model_checkpoint = tf.keras.callbacks.ModelCheckpoint('best.hdf5', save_best_only=True)
+model.fit(x=sequences, y=labels_reshaped, epochs=500, batch_size=5, validation_split=0.2,
+          callbacks=[early_stopping, model_checkpoint], shuffle=True)
 print('wytrenowano model')
 model.save('custom_keras.keras')
 print('zapisano model')
--- shot_detection/prepare_labels.py
+++ shot_detection/prepare_labels.py
@@ -3,6 +3,7 @@ import os
 
 FRAMES_PER_SHOT = 27
 
+
 def format_number(x):
     if x < 10:
         return f'00{x}'
@@ -20,18 +21,12 @@ def load_data(frames_path='dataset/frames', shots_path='dataset/shots'):
     labels = pd.DataFrame(columns=['path', 'is_shot'])
     for i in range(number_of_clips):
         shot_number = format_number(i)
-        for j in range(FRAMES_PER_SHOT - 2):
-            frames = [picture_string.format(shot_number=shot_number, frame_number=format_number(j)),
-                      picture_string.format(shot_number=shot_number, frame_number=format_number(j+1)),
-                      picture_string.format(shot_number=shot_number, frame_number=format_number(j+2))
-                      ]
+        for j in range(FRAMES_PER_SHOT):
+            frame = picture_string.format(shot_number=shot_number, frame_number=format_number(j))
             is_shot = 0
-            for frame in frames:
-                print(frame)
-                if frame in shots:
-                    is_shot = 1
-                    break
-            labels.loc[len(labels)] = [frames[0], is_shot]
+            if frame in shots:
+                is_shot = 1
+            labels.loc[len(labels)] = [frame, is_shot]
     labels.to_csv('labels.csv', index=False)
 
 if __name__ == "__main__":
